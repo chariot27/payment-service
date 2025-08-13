ca61c1dab2e448fee2af2a45d5bc0ebd15c077c9
chariot27 <maxtarigodev@gmail.com>
2025-08-13 17:38:51 -0300

    211

--- src/main/java/br/ars/payment_service/dto/CheckoutResponse.java
+++ src/main/java/br/ars/payment_service/dto/CheckoutResponse.java
@@ -1,11 +1,13 @@
+// br.ars.payment_service.dto.CheckoutResponse.java
 package br.ars.payment_service.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.OffsetDateTime;
 
 public record CheckoutResponse(
-        String txid,
-        String copiaECola,
-        String qrPngBase64,
-        String amount,
-        OffsetDateTime expiresAt
+    String txid,
+    @JsonProperty("copiaECola") String copiaECola,
+    String qrPngBase64,
+    String amount,
+    OffsetDateTime expiresAt
 ) {}
