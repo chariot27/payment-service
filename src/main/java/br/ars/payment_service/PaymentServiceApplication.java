352bb6e0b88874134f0cd650ff1a30250ffd0733
chariot27 <maxtarigodev@gmail.com>
2025-08-14 15:49:53 -0300

    pay

--- src/main/java/br/ars/payment_service/PaymentServiceApplication.java
+++ src/main/java/br/ars/payment_service/PaymentServiceApplication.java
@@ -3,9 +3,11 @@ package br.ars.payment_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableScheduling
+@CrossOrigin
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
